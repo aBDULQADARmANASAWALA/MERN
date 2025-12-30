c0ad2f46c19953ead5bb879af4cb845f37408fd2
aBDULQADARmANASAWALA <aqmanasawala@gmail.com>
2025-12-30 20:21:05 +0530

    updating metadata

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,7 +7,10 @@ const Navbar = () => {
         <header className='bg-base-300 border-b border-base-content/10'>
             <div className='mx-auto max-w-6xl p-4'>
                 <div className='flex items-center justify-between'>
-                    <h1 className='text-3xl font-bold text-primary font-mono tracking-tight'>MERN</h1>
+                    <div className='flex items-center gap-3'>
+                        <img src="/mernRookieLogo.svg" alt="MERN Rookie Logo" className='h-10 w-10' />
+                        <h1 className='text-3xl font-bold text-primary font-mono tracking-tight'>MERN Rookie</h1>
+                    </div>
                     <div className='flex items-center gap-4'>
                         <Link to={"/create"} className='btn btn-primary'>
                             <PlusIcon className='size-5' />
