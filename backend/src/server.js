8bc71aa44801060319b3752a2a87d677b4bfb75e
aBDULQADARmANASAWALA <aqmanasawala@gmail.com>
2025-12-30 19:22:26 +0530

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path, { resolve } from "path";
 
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
@@ -8,17 +9,34 @@ import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
+
 const app = express();
+const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 //middleware
-app.use(cors());
+if (process.env.NODE_ENV !== "production") {
+    app.use(
+        cors({
+            origin: "http://localhost:5173",
+        })
+    );
+}
 app.use(express.json());
 app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
+
 connectDB().then(() => {
-    app.listen(5001, () => {
-        console.log("Server is running on PORT: 5001");
+    app.listen(PORT, () => {
+        console.log("Server is running on PORT:", PORT);
     });
 }); 
